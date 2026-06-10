acbacdca898cacb86e98f729218fbc15b6f0672a
lucas <lucasltx2012@gmail.com>
2026-06-10 20:47:35 +0800

    Update signup.js

--- signup.js
+++ signup.js
@@ -2,23 +2,28 @@ import { auth, db } from "./firebase.js";
 import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-auth.js";
 import { doc, setDoc } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-firestore.js";
 
-document.getElementById("signupForm").addEventListener("submit", async (e)=>{
+document.getElementById("signupForm").addEventListener("submit", async (e) => {
     e.preventDefault();
 
-    const email = email.value;
-    const password = password.value;
-    const plate = plate.value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const plate = document.getElementById("plate").value;
 
-    const userCred = await createUserWithEmailAndPassword(auth,email,password);
+    try {
+        const userCred = await createUserWithEmailAndPassword(auth, email, password);
 
-    await setDoc(doc(db,"users",userCred.user.uid),{
-        email,
-        plate,
-        role:"user",
-        wallet:0,
-        points:0
-    });
+        await setDoc(doc(db, "users", userCred.user.uid), {
+            email: email,
+            plate: plate,
+            role: "user",
+            wallet: 0,
+            points: 0
+        });
 
-    alert("Account created!");
-    window.location.href="index.html";
+        alert("Account created!");
+        window.location.href = "index.html";
+
+    } catch (error) {
+        alert(error.message);
+    }
 });
