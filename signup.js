b01c48f5124f15acd71ede9be8d21eefb7f9982a
lucas <lucasltx2012@gmail.com>
2026-06-10 20:29:19 +0800

    Update signup.js

--- signup.js
+++ signup.js
@@ -1,1 +1,24 @@
+import { auth, db } from "./firebase.js";
+import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-auth.js";
+import { doc, setDoc } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-firestore.js";
 
+document.getElementById("signupForm").addEventListener("submit", async (e)=>{
+    e.preventDefault();
+
+    const email = email.value;
+    const password = password.value;
+    const plate = plate.value;
+
+    const userCred = await createUserWithEmailAndPassword(auth,email,password);
+
+    await setDoc(doc(db,"users",userCred.user.uid),{
+        email,
+        plate,
+        role:"user",
+        wallet:0,
+        points:0
+    });
+
+    alert("Account created!");
+    window.location.href="index.html";
+});
